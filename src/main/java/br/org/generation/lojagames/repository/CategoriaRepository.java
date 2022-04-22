763407670278fc45f7ff581451c3583197900c9d
Twany93 <twany.rocha@hotmail.com>
2022-04-22 18:27:20 -0300

    Loja de Games- CRUD + Security

--- src/main/java/br/org/generation/lojagames/repository/CategoriaRepository.java
+++ src/main/java/br/org/generation/lojagames/repository/CategoriaRepository.java
@@ -1,11 +1,12 @@
 package br.org.generation.lojagames.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.org.generation.lojagames.model.Categoria;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 	
 	public List<Categoria> findAllByTipoContainingIgnoreCase(String tipo);
--- src/main/java/br/org/generation/lojagames/repository/ProdutoRepository.java
+++ src/main/java/br/org/generation/lojagames/repository/ProdutoRepository.java
@@ -1,8 +1,11 @@
 package br.org.generation.lojagames.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import br.org.generation.lojagames.model.Produto;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 
 	Object findAllByNomeContainingIgnoreCase(String nome);
